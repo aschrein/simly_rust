617f220b702aca51f56a34ea38b1bc3b8f42fe2f
Anton Schreiner <antonschrein@gmail.com>
2024-07-11 13:10:37 +0200

    upd a bit broken

--- src/dsl.rs
+++ src/dsl.rs
@@ -1,4 +1,7 @@
-
+use std::borrow::BorrowMut;
+use std::io;
+use std::path::PathBuf;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::{any::Any, string};
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -176,10 +179,18 @@ impl TokenStream {
                 '"' | '\'' => {
                     flush_token(&mut self.tokens, &mut cur_str, line, col);
                     let start_col = col;
+                    // let mut skip_next = false; // skip next character if it is preceded by a backslash
                     while let Some(next_char) = string.chars().nth(idx + 1) {
                         if next_char == c {
+                            // if skip_next {
+                            // skip_next = false;
+                            // } else {
                             break;
+                            // }
                         }
+                        // else if next_char == '\\' {
+                        // skip_next = true;
+                        // }
                         cur_str.push(next_char);
                         if next_char == '\n' {
                             line += 1;
@@ -322,15 +333,15 @@ impl TokenStream {
         self.lines.get(line).map(|s| s.as_str())
     }
 
-    pub fn peek(&self) -> Option<&Token> {
-        self.tokens.get(self.pos)
+    pub fn peek(&self) -> Option<Token> {
+        self.tokens.get(self.pos).cloned()
     }
 
     pub fn next(&mut self) -> Option<Token> {
         if self.pos < self.tokens.len() {
             let token = self.tokens[self.pos].clone();
             self.pos += 1;
-            Some(token)
+            Some(token.clone())
         } else {
             None
         }
@@ -440,10 +451,255 @@ impl From<String> for TokenStreamInput {
     }
 }
 
+use std::rc::Rc;
+use std::{fmt, fs};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ExprType {
+    LITERAL,
+    UNARY,
+    BINARY,
+    PARANTHESIS,
+    CALL,
+}
+
+use std::cell::RefCell;
+
+#[derive(Clone)]
+pub struct Expr {
+    id: usize,
+    expr_type: ExprType,
+    token: Token,
+    args: Vec<Rc<RefCell<Expr>>>,
+}
+
+static NEXT_ID: AtomicUsize = AtomicUsize::new(0);
+
+impl Expr {
+    pub fn new(expr_type: ExprType, token: Token, args: Vec<Rc<RefCell<Expr>>>) -> Self {
+        let id = NEXT_ID.fetch_add(1, Ordering::SeqCst);
+
+        Expr {
+            id,
+            expr_type,
+            token,
+            args,
+        }
+    }
+
+    pub fn value(&self) -> &str {
+        &self.token.value
+    }
+
+    pub fn left(&self) -> Option<&Rc<RefCell<Expr>>> {
+        self.args.get(0)
+    }
+
+    pub fn right(&self) -> Option<&Rc<RefCell<Expr>>> {
+        self.args.get(1)
+    }
+
+    // find left most leaf node with priority less than or equal to a given priority for balancing
+    pub fn find_left_most_leaf(&self, priority: i32) -> Option<Rc<RefCell<Expr>>> {
+        if self.expr_type == ExprType::BINARY && get_operator_priority(self.value()) <= priority {
+            if let Some(left) = self.left() {
+                let left_leaf = left.borrow().find_left_most_leaf(priority);
+                if left_leaf.is_some() {
+                    return left_leaf;
+                }
+            }
+            // If no leaf found in left subtree, return self
+            return Some(Rc::new(RefCell::new(self.clone())));
+        }
+        None
+    }
+
+    pub fn to_str(&self, indent: usize) -> String {
+        let mut s = String::new();
+
+        // Add indentation
+        s.push_str(&" ".repeat(indent));
+
+        // Add the current node's information
+        s.push_str(&format!("{:?} ", self.expr_type));
+        s.push_str(self.value());
+        s.push('\n');
+
+        // Add left child if it exists
+        if let Some(left) = self.left() {
+            s.push_str(&left.borrow().to_str(indent + 2));
+        }
+
+        // Add right child if it exists
+        if let Some(right) = self.right() {
+            s.push_str(&right.borrow().to_str(indent + 2));
+        }
+
+        s
+    }
+
+    // Write dot graph format for this expression
+    pub fn _to_dot(&self) -> String {
+        let mut s: String = String::new();
+        s = s + "subgraph {\n";
+        s = s + format!("node{} [label=\"{}\"];\n", self.id, self.value()).as_str();
+        for arg in &self.args {
+            s = s + format!("node{} -> node{};\n", self.id, arg.borrow().id).as_str();
+            s = s + arg.borrow()._to_dot().as_str();
+        }
+        s = s + "}\n";
+        s
+    }
+    pub fn to_dot(&self) -> String {
+        let mut s: String = String::new();
+        s = s + "digraph {\n";
+        s = s + self._to_dot().as_str();
+        s = s + "}\n";
+        s
+    }
+}
+
+impl fmt::Debug for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.to_str(0))
+    }
+}
+
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+// make a expression priority table for operators
+lazy_static! {
+    static ref OPERATOR_PRIORITY: HashMap<&'static str, i32> = {
+        let mut priority = HashMap::new();
+        priority.insert("||", 1);
+        priority.insert("&&", 2);
+        priority.insert("==", 3);
+        priority.insert("!=", 3);
+        priority.insert("<", 4);
+        priority.insert(">", 4);
+        priority.insert("<=", 4);
+        priority.insert(">=", 4);
+        priority.insert("+", 5);
+        priority.insert("-", 5);
+        priority.insert("*", 6);
+        priority.insert("/", 6);
+        priority.insert("%", 6);
+        priority
+    };
+}
+
+fn get_operator_priority(op: &str) -> i32 {
+    *OPERATOR_PRIORITY.get(op).unwrap_or(&0)
+}
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub fn recursive_parse_expression(tk: &mut TokenStream) -> Option<Rc<RefCell<Expr>>> {
+    let c = tk.next()?;
+
+    if c.value == "(" {
+        let inner_expr = recursive_parse_expression(tk)?;
+        let p = Rc::new(RefCell::new(Expr::new(
+            ExprType::PARANTHESIS,
+            c.clone(),
+            vec![inner_expr],
+        )));
+        assert!(tk.consume(")"), "Expected ')' but got {:?}", tk.peek());
+        return Some(p);
+    }
+
+    if let Some(n) = tk.peek() {
+        if n.value == "(" {
+            tk.next();
+            let inner_expr = recursive_parse_expression(tk)?;
+            let p = Rc::new(RefCell::new(Expr::new(
+                ExprType::CALL,
+                c.clone(),
+                vec![inner_expr],
+            )));
+            assert!(tk.consume(")"), "Expected ')' but got {:?}", tk.peek());
+            return Some(p);
+        } else if n.token_type == TokenType::OPERATOR {
+            tk.next();
+            let left = Rc::new(RefCell::new(Expr::new(
+                ExprType::LITERAL,
+                c.clone(),
+                vec![],
+            )));
+            let mut right = recursive_parse_expression(tk)?;
+
+            // Balance the expression tree
+            {
+                let right_borrow = right.borrow();
+                if right_borrow.expr_type == ExprType::BINARY {
+                    let right_priority = get_operator_priority(right_borrow.value());
+                    let this_priority = get_operator_priority(&n.value);
+                    if right_priority < this_priority {
+                        // We need to rotate, but first we need to drop the borrow
+                        drop(right_borrow);
+
+                        // Now we can mutate
+                        let mut right_mut = right.borrow_mut();
+                        let right_left = right_mut.args[0].clone();
+                        right_mut.args[0] = left.clone();
+
+                        // We need to drop the mutable borrow before reassigning 'right'
+                        drop(right_mut);
+
+                        return Some(Rc::new(RefCell::new(Expr::new(
+                            ExprType::BINARY,
+                            n.clone(),
+                            vec![right, right_left],
+                        ))));
+                    }
+                }
+            } // right_borrow goes out of scope here
+
+            return Some(Rc::new(RefCell::new(Expr::new(
+                ExprType::BINARY,
+                n.clone(),
+                vec![left, right],
+            ))));
+        }
+    }
+
+    Some(Rc::new(RefCell::new(Expr::new(
+        ExprType::LITERAL,
+        c.clone(),
+        vec![],
+    ))))
+}
+
+pub fn get_or_create_tmp_folder() -> io::Result<PathBuf> {
+    let current_dir = std::env::current_dir()?;
+    let tmp_dir = current_dir.join(".tmp");
+
+    if !tmp_dir.exists() {
+        fs::create_dir(&tmp_dir)?;
+    }
+
+    Ok(tmp_dir)
+}
+
+use std::io::Write;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_expr() {
+        let input = "1 + 2 * 3 - 4 / 5".to_string();
+        let mut stream = TokenStream::new(input, None).unwrap();
+        let expr = recursive_parse_expression(&mut stream).unwrap();
+        println!("{:?}", expr);
+
+        let tmp_file = get_or_create_tmp_folder().unwrap().join("test.dot");
+        let mut file = fs::File::create(&tmp_file).unwrap();
+        file.write_all(expr.to_dot().as_bytes()).unwrap();
+    }
 
     #[test]
     fn test_tokenize() {
@@ -464,10 +720,20 @@ mod tests {
 
     #[test]
     fn test_tokenize_string() {
-        let input = r#""Hello World" + 'Hello World' + "Hello World" + 'Hello World'"#.to_string();
+        let input = r#"
+        
+        "Hello World" + 'Hello World' + "Hello World" + 'Hello World'
+        
+        
+        "#
+        .to_string();
         let mut stream = TokenStream::new(input, None).unwrap();
         let tokens = stream.tokens;
-        assert_eq!(tokens.len(), 9);
+        // println!("{:?}", tokens);
+        // for token in &tokens {
+        // println!("{:?}", token);
+        // }
+        assert_eq!(tokens.len(), 7);
         assert_eq!(tokens[0].value, "Hello World");
         assert_eq!(tokens[1].value, "+");
         assert_eq!(tokens[2].value, "Hello World");
@@ -479,7 +745,11 @@ mod tests {
 
     #[test]
     fn test_tokenize_string_with_escape() {
-        let input = r#""Hello \"World\"""#.to_string();
+        let input = r#"
+        'Hello "World"'
+        
+        "#
+        .to_string();
         let mut stream = TokenStream::new(input, None).unwrap();
         let tokens = stream.tokens;
         assert_eq!(tokens.len(), 1);
@@ -488,11 +758,13 @@ mod tests {
 
     #[test]
     fn test_tokenize_string_with_escape_single_quote() {
-        let input = r#"'Hello \'World\''"#.to_string();
+        let input = r#"
+        "Hello 'World'"
+        "#
+        .to_string();
         let mut stream = TokenStream::new(input, None).unwrap();
         let tokens = stream.tokens;
         assert_eq!(tokens.len(), 1);
         assert_eq!(tokens[0].value, "Hello 'World'");
     }
-
 }
